f3fc7f4db4d9addd6bd00edf7271a60cabe8d8b0
Peter Siroki <310489+psiroki@users.noreply.github.com>
2021-01-23 20:26:55 +0100

    Added horizontal mode

--- carp.js
+++ carp.js
@@ -1,15 +1,17 @@
 let video = document.querySelector("#videoElement");
 let info = document.getElementById("info");
 let flip = false;
-let canvas = null;
+let canvasDiv = null;
 let blob = null;
 let blobUrl = null;
 let blobAnchor = null;
+let horizontal = false;
 
 function updateFlip() {
   let transform = flip ? "scaleX(-1)" : "";
-  video.style.transform = transform;
-  if (canvas) canvas.style.transform = transform;
+  document.body.classList.toggle("flipped", flip);
+  if (canvasDiv) canvasDiv.style.transform = transform;
+  if (blobAnchor) blobAnchor.style.transform = transform;
 }
 
 function setFlip(val) {
@@ -36,6 +38,7 @@ if (navigator.mediaDevices.getUserMedia) {
     video: {
       width: { min: wm[location.hash] || 320, ideal: 1920 },
       height: { min: hm[location.hash] || 180, ideal: 1080 },
+      frameRate: { min: 30, ideal: 60 },
       facingMode: fm[location.hash]
     }
   };
@@ -50,10 +53,50 @@ if (navigator.mediaDevices.getUserMedia) {
     });
 }
 
+function animate(fun) {
+  let stop = false;
+  const controller = {
+    lastTimestamp: null,
+    stop() {
+      stop = true;
+    }
+  };
+  let exec = (...args) => {
+    if (!stop) {
+      controller.lastTimestamp = args[0];
+      let result = fun(controller, ...args);
+      if (typeof result !== "boolean" || result) {
+        requestAnimationFrame(exec);
+      }
+    }
+  };
+  requestAnimationFrame(exec);
+  return controller;
+}
+
+function copyRect(ctx, src, r) {
+  const x = r[0];
+  const y = r[1];
+  const w = r[2] - r[0];
+  const h = r[3] - r[1];
+  ctx.drawImage(src, x, y, w, h, x, y, w, h);
+}
+
+function fillRect(ctx, r) {
+  const x = r[0];
+  const y = r[1];
+  const w = r[2] - r[0];
+  const h = r[3] - r[1];
+  ctx.fillRect(x, y, w, h);
+}
+
 video.addEventListener("click", function() {
 //  setFlip(!flip);
-  if (canvas) canvas.remove();
-  canvas = document.createElement("canvas");
+  if (canvasDiv) canvasDiv.remove();
+  canvasDiv = document.createElement("div");
+  canvasDiv.classList.add("canvas");
+  let canvas = document.createElement("canvas");
+  canvasDiv.appendChild(canvas);
   updateFlip();
   blob = null;
   if (blobUrl) {
@@ -63,18 +106,32 @@ video.addEventListener("click", function() {
   if (blobAnchor) blobAnchor.remove();
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
-  document.body.appendChild(canvas);
+  document.body.appendChild(canvasDiv);
   let ctx = canvas.getContext("2d");
-  let y = 0;
+  let pos = 0;
   let a = null;
-  let intervalId = setInterval(() => {
-    let startY = y;
-    y += 5;
-    ctx.drawImage(video,
-      0, startY, video.videoWidth, y - startY,
-      0, startY, video.videoWidth, y - startY);
-    if (y >= video.videoHeight) {
-      clearInterval(intervalId);
+  let lastTime = null;
+  let progressSize = horizontal ? video.videoWidth : video.videoHeight;
+  let otherSize = horizontal ? video.videoHeight : video.videoWidth;
+  let progressDim = horizontal ? 0 : 1;
+  let otherDim = horizontal ? 1 : 0;
+  let anim = animate((anim, t) => {
+    if (lastTime === null) {
+      lastTime = t;
+      return;
+    }
+    const timeDelta = t - lastTime;
+    let start = pos|0;
+    pos += timeDelta * 5e-3;
+    let end = pos|0;
+    let rect = Array(4).fill(0);
+    rect[otherDim] = 0;
+    rect[otherDim + 2] = otherSize;
+    rect[progressDim] = start;
+    rect[progressDim + 2] = end;
+    copyRect(ctx, video, rect);
+    if (end >= progressSize) {
+      anim.stop();
       canvas.toBlob(b => {
         if (blobAnchor) blobAnchor.remove();
         a = document.createElement("a");
@@ -83,16 +140,31 @@ video.addEventListener("click", function() {
         a.href = blobUrl;
         a.textContent = "Save";
         a.download = "carp_"+(Math.random()*256*1048576|0).toString(16).padStart(7, "0")+".jpg";
-        document.body.appendChild(a);
+        canvasDiv.appendChild(a);
         blobAnchor = a;
+        updateFlip();
       }, "image/jpeg", 0.7);
+    } else {
+      rect[progressDim] = end + 1;
+      rect[progressDim + 2] = 2 * end - start + 1;
+      ctx.fillStyle = "#0008";
+      fillRect(ctx, rect);
+      ctx.fillStyle = "#4f48";
+      ++rect[progressDim];
+      --rect[progressDim + 2];
+      fillRect(ctx, rect);
     }
-  }, 33);
-  canvas.addEventListener("click", e => {
-    clearInterval(intervalId);
+  });
+  canvasDiv.addEventListener("click", e => {
+    anim.stop();
     e.currentTarget.remove();
     if (a) a.remove();
   });
+});
+
+document.querySelector("button").addEventListener("click", _ => {
+  horizontal = !horizontal;
+  document.body.classList.toggle("horizontal", horizontal);
 });
 
 video.addEventListener("loadeddata", function (e) {
